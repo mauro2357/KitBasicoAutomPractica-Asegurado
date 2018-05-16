d79ff4aa7a68d17d9192f2e464c840009a1d771a
NP375118 <NP375118@I5GMD033.pe.intranet>
2018-05-16 09:21:39 -0500

    "Se cambio el controler AfiliadosController por AseguradorController"

--- KitBasicoAutomApp/src/main/java/co/com/valtica/kitbasico/controller/AseguradoController.java
+++ KitBasicoAutomApp/src/main/java/co/com/valtica/kitbasico/controller/AseguradoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class AfiliadosController {
+public class AseguradoController {
 /*
 	 @Autowired
 	 IAfiliadosFacade afiliadosFacade;*/
